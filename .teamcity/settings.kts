214dfc6b4c066b5d69f834e2ca8547fb86e66382
admin <admin@TeamCity>
2026-07-20 17:36:15 +0000

    Synchronization with own VCS root is enabled (TeamCity change in 'Netology TeamCity' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,62 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2026.1"
+
+project {
+    description = "TeamCity homework for Netology"
+
+    buildType(ExampleTeamCity)
+}
+
+object ExampleTeamCity : BuildType({
+    name = "Example TeamCity"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            name = "Maven test"
+            id = "Maven_test"
+
+            conditions {
+                doesNotEqual("teamcity.build.branch", "master")
+            }
+            goals = "clean test"
+        }
+        maven {
+            name = "Maven deploy"
+            id = "Maven_deploy"
+
+            conditions {
+                equals("teamcity.build.branch", "master")
+            }
+            goals = "clean deploy"
+            userSettingsSelection = "settings.xml"
+        }
+    }
+})
